ebf7352606a7193c6fb1008e42beadc351e6d172
abing <mac@abing-2.local>
2020-06-26 07:57:17 +0800

    ride

---
+++ _posts/store_10/template.php
@@ -0,0 +1,33 @@
+<?php
+!defined('IN_MUDDER') && exit('Access Denied');
+return array (
+  'about.htm' => '主题关于',
+  'album.htm' => '主题相册',
+  'article_detail.htm' => '文章内页',
+  'article_list.htm' => '文章列表',
+  'ask_detail.htm' => '问答内页',
+  'ask_list.htm' => '问答列表',
+  'coupon_detail.htm' => '优惠券内页',
+  'coupon_list.htm' => '优惠券列表',
+  'fenlei_detail.htm' => '分类信息内页',
+  'fenlei_list.htm' => '分类信息列表',
+  'footer.htm' => '底部模板',
+  'group_list.htm' => '小组列表',
+  'group_show.htm' => '小组话题列表',
+  'group_topic.htm' => '小组话题内页',
+  'guestbook_list.htm' => '主题留言',
+  'header.htm' => '头部模板',
+  'index.htm' => '主题首页',
+  'party_detail.htm' => '聚会活动内容页',
+  'party_list.htm' => '聚会活动列表页',
+  'part_forum.htm' => '主题首页附属，论坛帖子列表',
+  'part_guestbook.htm' => '主题首页附属，留言列表',
+  'part_review.htm' => '主题首页附属，点评列表',
+  'part_side.htm' => '主题侧边栏',
+  'pic.htm' => '主题图片内页',
+  'product_detail.htm' => '产品内页',
+  'product_list.htm' => '产品列表',
+  'review_list.htm' => '点评列表',
+  'style.css' => '主题风格css文件',
+); 
+?>
